3eaa0eccf80849040cfd6424615dae69b046deb7
bAbderraouf <87968764+bAbderraouf@users.noreply.github.com>
2025-06-20 23:05:50 +0200

    grid.h

--- Grid.h
+++ Grid.h
@@ -22,9 +22,6 @@ public:
 	void DrawOneCell(int x, int y, int w, int h, Color color);
 	int getNumRows();
 	int getNumCols();
-	int GetIndexOfFirstNotEmptyLine();
-	bool CheckIfLineIsFull(int lineIndex);
-	void DeleteFullLines();
 	void SetMargins(int left, int top);
 	int grid[20][10]; // 20 rows and 10 collumns
 
